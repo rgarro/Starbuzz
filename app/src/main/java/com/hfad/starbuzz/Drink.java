225873fe76d0bbe88d71e83b529ea584ad1738ef
Rolando <rgarro@gmail.com>
2026-06-27 13:21:33 -0600

    the starbuzz coffe project

--- app/src/main/java/com/hfad/starbuzz/Drink.java
+++ app/src/main/java/com/hfad/starbuzz/Drink.java
@@ -18,6 +18,12 @@ package com.hfad.starbuzz;
  * esta se vuelve viva y aparece como el pequeño valle de Tirrases ...
  * La Mierda de Breda fue puesta en todas sus cuevas en Coronado
  * Sir Henry Morgan ha venido a recaudar el sueldo del Tercio Gales que no han pagado.
+ * Los Testigos de JHehova deben irse , su rey ha murido en la guerra
+ * CostaRica era una provincia de siervos Incas testigos de jehova
+ * Henry Morgan ha decapitado a todos lo que no se bautizaron en el nevo Cartago();
+ * los vendio al Papa en Milan y el sueldo del tercio pago.
+ *Los dejaban secar caffe con cannabis de Algeria para que recordaran que fueron enemigos deben hablar español
+ *
  *
  *
  *
@@ -36,4 +42,27 @@ public class Drink {
             new Drink("Cappuccino","Espresso, hot milk and steamed milk foam",R.drawable.capuccino),
             new Drink("Filter","Highest quality beans roasted and brewed fresh",R.drawable.filter)
     };
+
+    //each Drink has a name description and img resource
+    private Drink(String name,String description,int imageResourceId){
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getImageResourceId(){
+        return imageResourceId;
+    }
+
+    public String toString(){
+        return this.name;
+    }
 }
