f64ec8b9489700981d26a82c761273fdd39947af
Rolando <rgarro@gmail.com>
2026-06-29 16:33:55 -0600

    baia dos tigres mystic

--- app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -2,6 +2,11 @@ package com.hfad.starbuzz;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
 /**
  *            |    |    |
  *           )_)  )_)  )_)
@@ -17,11 +22,12 @@ import android.app.Activity;
  * in front of the Viscaya Beach, La Noche fue abandona y todos
  * prepararon batalla en La Ciudad que esperaba ser auxiliada por EL Sol
  * Me cago en su Dios hijueputas!!!  gritaron a toda voz en el castillo de Proa en La Ciudad
- * Al Final la Cuidad quedo dormida ..
+ * Al Final la Cuidad quedo dormida , El Sol no aparecio ..
  * The Harlots reading the king James bible at BlueFields ....
  *
  *
- * 
+ *
+ *
  *
  * @author Rolando <rgarro@gmail.com>
  */
@@ -31,6 +37,11 @@ public class DrinkCategoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                Drink.drinks);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(listAdapter);
     }
 
 }
