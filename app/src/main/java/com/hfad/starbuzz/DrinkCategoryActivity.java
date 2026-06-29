9522db94ef88c2fca7adeb27350e0a2697bc5b29
Rolando <rgarro@gmail.com>
2026-06-29 16:10:51 -0600

    al final la noche quedo dormida y talvezte vuelva a encontrar ...

---
+++ app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,36 @@
+package com.hfad.starbuzz;
+
+import android.os.Bundle;
+import android.app.Activity;
+/**
+ *            |    |    |
+ *           )_)  )_)  )_)
+ *          )___))___))___)\
+ *         )____)____)_____)\\
+ *      _____|____|____|____\\\__
+ * -------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *           ^^^^      ^^^
+ * Sir Henry Morgan is de Lord of Talamanca
+ * The Subversion Tortoise, intercepted two Spanish vessels
+ * in front of the Viscaya Beach, La Noche fue abandona y todos
+ * prepararon batalla en La Ciudad que esperaba ser auxiliada por EL Sol
+ * Me cago en su Dios hijueputas!!!  gritaron a toda voz en el castillo de Proa en La Ciudad
+ * Al Final la Cuidad quedo dormida ..
+ * The Harlots reading the king James bible at BlueFields ....
+ *
+ *
+ * 
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
+public class DrinkCategoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+    }
+
+}
