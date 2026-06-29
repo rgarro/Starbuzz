1839da71f5d5d92711aba1c3d7a61448c8b60cbc
Rolando <rgarro@gmail.com>
2026-06-29 13:13:42 -0600

    projec update ...

--- app/src/main/java/com/hfad/starbuzz/Drink.java
+++ app/src/main/java/com/hfad/starbuzz/Drink.java
@@ -12,17 +12,7 @@ package com.hfad.starbuzz;
  *           ^^^^      ^^^
  * Sir Henry Morgan is de Lord of Talamanca
  * load the starboard guns, muskets on the stern
- * la Dulceje fue decapitada en Cot de Cartago, Hijueputas!!
- * Cartago was a market inside of a shooting star landing cave
- * Cuando muchos mean y han cagado dentro de una cueva con cara oculta
- * esta se vuelve viva y aparece como el pequeño valle de Tirrases ...
- * La Mierda de Breda fue puesta en todas sus cuevas en Coronado
- * Sir Henry Morgan ha venido a recaudar el sueldo del Tercio Gales que no han pagado.
- * Los Testigos de JHehova deben irse , su rey ha murido en la guerra
- * CostaRica era una provincia de siervos Incas testigos de jehova
- * Henry Morgan ha decapitado a todos lo que no se bautizaron en el nevo Cartago();
- * los vendio al Papa en Milan y el sueldo del tercio pago.
- *Los dejaban secar caffe con cannabis de Algeria para que recordaran que fueron enemigos deben hablar español
+ * Mamerto es playo Homosexual Walter solis gay de chino leyo aqui <-
  *
  *
  *
