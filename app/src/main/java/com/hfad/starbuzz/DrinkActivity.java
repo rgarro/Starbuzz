147cfed6c07ced56709d10a9a0eb8fb6c7deda5a
Rolando <rgarro@gmail.com>
2026-07-03 13:10:05 -0600

    polly wants a cracker maybe she would like some food ..

--- app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -2,6 +2,8 @@ package com.hfad.starbuzz;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.ImageView;
+import android.widget.TextView;
 /**
  *            |    |    |
  *           )_)  )_)  )_)
@@ -13,7 +15,9 @@ import android.app.Activity;
  *     ^^^^      ^^^^     ^^^    ^^
  *           ^^^^      ^^^
  * Sir Henry Morgan is de Lord of Talamanca
- *
+ * tossing with brave men having having sold Panama to Rossevelt
+ * Eventually Jamaica got invaded by Cuban Namibian Black Sorcerer named Bob Marley
+ * who bought a Building built with wood comming from the Black Swan where he has a music recording studio
  *
  *
  *
@@ -21,10 +25,28 @@ import android.app.Activity;
  */
 public class DrinkActivity extends Activity {
 
+    public static final String EXTRA_DRINKID = "drinkId";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+        //get drink from intent
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
+        Drink drink = Drink.drinks[drinkId];
+
+        //populate the drink name
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText(drink.getDescription());
+
+        //populate the drink description
+        TextView description = (TextView) findViewById(R.id.description);
+        description.setText(drink.getDescription());
+
+        //populate the drink image
+        ImageView photo = (ImageView) findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
     }
 
 }
