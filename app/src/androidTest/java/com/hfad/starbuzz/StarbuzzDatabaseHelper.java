027ec03d44539da6498894e1807afab4d7468ab0
Rolando <rgarro@gmail.com>
2026-07-22 19:28:36 -0600

    compiling update

--- app/src/androidTest/java/com/hfad/starbuzz/StarbuzzDatabaseHelper.java
+++ app/src/androidTest/java/com/hfad/starbuzz/StarbuzzDatabaseHelper.java
@@ -26,7 +26,7 @@ import android.content.ContentValues;
 public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "starbuzz";
-    private static final int DB_VERSION = 2;
+    private static final int DB_VERSION = 1;
 
     StarbuzzDatabaseHelper(Context context){
         super(context,DB_NAME,null,DB_VERSION);
